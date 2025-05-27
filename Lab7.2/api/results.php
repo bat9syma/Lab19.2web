49af87b234221d067d1f506123454227908614a4
bat9syma <133958260+bat9syma@users.noreply.github.com>
2025-05-28 00:35:56 +0300

    Update results.php

--- Lab7.2/api/results.php
+++ Lab7.2/api/results.php
@@ -3,7 +3,7 @@ $filePath = '/tmp/symovych.txt';
 $file = fopen($filePath, 'w');
 
 if (!$file) {
-    echo "❌ Cannot open file for writing at: $filePath";
+    echo "Cannot open file for writing at: $filePath";
     exit;
 }
 ?>
