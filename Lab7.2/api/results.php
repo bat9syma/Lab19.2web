b550189fc9de9e1ea56e7e4b97e2bdc022cd6074
bat9syma <133958260+bat9syma@users.noreply.github.com>
2025-05-28 00:20:59 +0300

    Update results.php

--- Lab7.2/api/results.php
+++ Lab7.2/api/results.php
@@ -1,5 +1,5 @@
 <?php
-$filePath = dirname(__DIR__) . '/data/symovych.txt';
+$filePath = __DIR__ . '/../data/symovych.txt';
 
 if (file_exists($filePath)) {
     echo nl2br(htmlspecialchars(file_get_contents($filePath)));
