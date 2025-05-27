71be0fd66187a2201b69be9892ef58f0cc7369db
bat9syma <133958260+bat9syma@users.noreply.github.com>
2025-05-28 02:02:27 +0300

    Update results.php

--- Lab7.2/api/results.php
+++ Lab7.2/api/results.php
@@ -7,25 +7,3 @@ if (!file_exists($tempPath)) {
     exit;
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="uk">
-<head>
-  <meta charset="UTF-8">
-  <title>Результати обчислень</title>
-</head>
-<body>
-  <h2>Результати</h2>
-
-  <p style="color: green;"> Файл успішно записано у тимчасову папку: <code><?= htmlspecialchars($tempPath) ?></code></p>
-
-  <h3>Вміст файлу:</h3>
-  <pre><?= htmlspecialchars(file_get_contents($tempPath)) ?></pre>
-
-  <form method="post" action="download.php">
-    <input type="hidden" name="filename" value="symovych.txt">
-    <button type="submit">⬇ Завантажити файл</button>
-  </form>
-
-</body>
-</html>
