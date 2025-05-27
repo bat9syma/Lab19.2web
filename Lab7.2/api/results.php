3abf336cbc4960bd368b858c251e00b408e1ff01
bat9syma <133958260+bat9syma@users.noreply.github.com>
2025-05-28 00:41:46 +0300

    Update results.php

--- Lab7.2/api/results.php
+++ Lab7.2/api/results.php
@@ -2,10 +2,15 @@
 $filePath = '/tmp/symovych.txt';
 
 if (file_exists($filePath)) {
+    header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
     header('Content-Disposition: attachment; filename="symovych.txt"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($filePath));
     readfile($filePath);
     exit;
 } else {
-    echo "File not found: $filePath";
-?>
+    echo "Файл не знайдено :(";
+}
